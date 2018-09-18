b678d39dd0cf4ae95b9646d138059d271a41c087
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-18 13:43:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function upperCaseCustomerName() {
 }
 
 function setBestCustomer() {
-  let bestCustomer = `not ${customerName}`;
+  var bestCustomer = `not ${customerName}`;
   return bestCustomer;
 }
