158693ad362c80795c283973ae617cf3c080d7d2
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-18 13:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution in this file!
 var customerName = "bob"
 function upperCaseCustomerName(customerName){
-  let name = customerName.toUpperCase()
+  let name = customerName.toUpperCase();
+  return name;
 }
